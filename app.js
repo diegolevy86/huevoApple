744cca71276ba7ec6bfae7355354842f5a4fb13a
Diego Levy <dielevy@gmail.com>
2023-07-20 17:26:21 -0300

    Se agregó la función buscar Reparacion por imei

--- app.js
+++ app.js
@@ -47,6 +47,36 @@ app.post("/buscarBDfecha", async function (req, res) {
     res.render("buscarBDfecha", { pack: pack });
 });
 
+app.post("/buscarBDimei", async function (req, res) {
+    let pack = []
+    let data = await database.buscarBDimei(req.body.imei);
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        let row = []
+        row.push(data[i].idReparacion)
+        row.push(data[i].IMEI)
+        let dato = await database.buscarModeloColor(data[i].idModeloColor);
+        row.push(dato[0])
+        row.push(dato[1])
+        row.push(data[i].Cliente)
+        row.push(data[i].Email)
+        row.push(data[i].Telefono)
+        row.push(data[i].Codigo)
+        row.push(data[i].Fecha)
+        row.push(data[i].Reparacion)
+        row.push(data[i].Observaciones)
+        row.push(data[i].Reventa)
+        if (data[i].Garantia) {
+            row.push("Sí");
+        }
+        else {
+            row.push("");
+        }
+        pack.push(row);
+    }
+    res.render("buscarBDimei", { pack: pack });
+});
+
 app.listen(port, function () {
     console.log("Server started on port " + port);
 });
